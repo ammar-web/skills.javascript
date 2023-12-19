fb5eab1a537174953470137044f9cf318179a4c2
Coding Knight <141774089+ammar-web@users.noreply.github.com>
2023-12-19 22:17:34 +0530

    Add files via upload

---
+++ Drum Kit  javascript file.js
@@ -0,0 +1,19 @@
+
+
+
+var drumbeats=document.querySelectorAll(".drum").length
+
+ for (var i=0;i<drumbeats;i++){
+
+
+document.querySelectorAll(".drum")[i].addEventListener("click",sounds)
+function sounds() {
+   
+    alert("i got clicked") 
+
+}
+}
+   
+
+
+
